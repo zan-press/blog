80595619aa636978d8840a54f22d211996b99c46
Garvey98 <864229420@qq.com>
2019-11-07 22:20:49 +0800

    Add blog (Week 4-2)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,6 +35,9 @@ module.exports = {
         }, {
           text: '第四周任务分工',
           link: '/Blog/Week4-1.md'
+        }, {
+          text: '第四周小组会议概要',
+          link: '/Blog/Week4-2.md'
         },]
       },
       {
@@ -54,7 +57,8 @@ module.exports = {
         'Week2-2.md',
         'Week3-1.md',
         'Week3-2.md',
-        'Week4-1.md'
+        'Week4-1.md',
+        'Week4-2.md'
       ]
     },
     lastUpdated: '最后更新于',
